f31a8821cb204ac51b8f750f8198e91a07cb611a
Rahul R Kaladgi <127622361+RAHULKALADGI@users.noreply.github.com>
2023-08-04 13:07:17 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,41 @@
-
 import React from "react";
 import './../styles/App.css';
+import {Routes , Route} from "react-router-dom";
 
-const App = () => {
+const App =()=> {
+
+return (
+  <div>
+    <ul>
+      <li>
+      <a href="/">Home</a>
+      </li>
+      <li>
+      <a href="/about">About</a>
+      </li>
+    </ul>
+    
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/about" element={<About/>}></Route>
+    </Routes>
+  </div>
+)
+
+}
+
+const Home = ()=> {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Welcome to my website!</h1>
+    </div>
+  )
+}
+const About = ()=> {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>This is the sample React Router program.</p>
     </div>
   )
 }
